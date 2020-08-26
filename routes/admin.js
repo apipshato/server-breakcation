f8b8a0f5e9b0cac51fc92ce7cb8396fae6c931b0
apipshato <afifshatos@gmail.com>
2020-08-26 14:58:32 +0700

    first commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
-const adminController =require('../controller/adminController')
+const adminController =require('../controller/adminController');
+const { viewBank, addCategory } = require('../controller/adminController');
+const { route } = require('.');
 
 router.get('/dashboard', adminController.viewDasboard);
+//endpoint category
+router.get('/category', adminController.viewCategory);
+router.post('/category',adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id',adminController.deleteCategory);
+router.get('/bank', adminController.viewBank);
+router.get('/item', adminController.viewItem);
+router.get('/booking', adminController.viewBooking);
 
 module.exports = router
