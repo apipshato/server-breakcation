98bc9e2f57502446dd7bcd3d2ed18a5da4a55deb
apipshato <afifshatos@gmail.com>
2020-09-14 21:48:29 +0700

    booking page and api

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const apiController =require('../controller/apiController');
 
 
-//const {uploadSingle, uploadMultiple}= require('../middleware/multer');
+const {uploadSingle}= require('../middleware/multer');
 
 
 
 //enpoint Signin
 
 router.get('/landing-page', apiController.landingPage);
+router.get('/detail-page/:id', apiController.detailPage);
+router.post('/booking-page',uploadSingle, apiController.bookingPage);
 module.exports = router;
